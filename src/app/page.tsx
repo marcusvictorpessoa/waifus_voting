19b14b61661a4b34e2a7407698f21af6786b037e
Marcus Pessoa <marcusvictor00@gmail.com>
2024-10-27 00:01:14 -0300

    chore: algumas melhorias no estilo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="w-[100vw] h-[100vh]">
       <Header />
       <div className="flex justify-center items-center w-[100%] h-[90%] bg-[url(/snow.gif)] bg-no-repeat bg-cover font-[family-name:var(--font-geist-sans)]">
-        <div className="flex flex-col justify-center items-center gap-2 max-sm:w-[70%] sm:w-[40%] h-[60%] backdrop-blur-lg shadow-md rounded-md">
+        <div className="flex flex-col justify-center items-center gap-2 max-sm:w-[80%] sm:w-[40%] h-[50%] backdrop-blur-lg shadow-md rounded-md">
           <span className="text-black text-center w-[70%] text-lg bg-white rounded-sm p-1">Vote en la waifu que usted gustou, pero consciente!</span>
           <input className="border text-lg border-black rounded-sm w-[70%] p-1" type="text" placeholder="Digite aqui su nombre" />
           <button className="border text-white border-[#f89fa1] rounded-sm bg-[#f89fa1] w-[70%] p-1 hover:bg-[#fabdbd]">VOTAR</button>
