329873f726daeba9df9b6f5116ea0406a4c74b41
Marcus Pessoa <marcusvictor00@gmail.com>
2024-10-29 18:36:33 -0300

    feat: labels

--- src/app/waifus/page.tsx
+++ src/app/waifus/page.tsx
@@ -7,7 +7,7 @@ export default function Waifus() {
       {false && <BackgroundSpinner />}
       <Header />
       <div className="overflow-y-scroll p-2 w-[100%] h-[90%] bg-[url(/snow.gif)] bg-no-repeat bg-cover font-[family-name:var(--font-geist-sans)]">
-      <span>Vote</span>
+      <span className="text-black text-center text-lg bg-white rounded-sm p-1">Pretendientes</span>
         {[1, 2, 3, 4, 5].map((waifu) => {
           return (
             <div key={waifu} className="mt-4 max-sm:w-[100%] sm:w-[100%] h-[250px] backdrop-blur-lg shadow-md rounded-md">
