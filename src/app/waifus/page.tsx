698304dad8626007381b6b39eaffbbb425770611
Marcus Pessoa <marcusvictor00@gmail.com>
2024-10-30 19:15:18 -0300

    fix: reference key

--- src/app/waifus/page.tsx
+++ src/app/waifus/page.tsx
@@ -6,17 +6,25 @@ import { useModal } from "@/context/modal";
 import { useVotante } from "@/context/votante";
 import useWaifus from "@/hooks/useWaifus";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 export default function Waifus() {
 
   const { waifus, loading } = useWaifus();
   const { openModal, modal } = useModal()
+  const keyVote = useRef("")
 
   const { name } = useVotante()
 
-  const idDelVotante = localStorage.getItem('votante')
+  useEffect(() => {
+    const idDelVotante = localStorage.getItem('votante');
+    const generateKeyVote = `${name}-${idDelVotante}`;
+    keyVote.current = generateKeyVote;
+  },[])
 
-  const newKeyVote = `${name}-${idDelVotante}`
+  
+
+  
 
   return (
     <main className="w-[100vw] h-[100vh]">
@@ -27,7 +35,7 @@ export default function Waifus() {
         <span className="text-black text-center text-lg bg-white rounded-sm p-1">Pretendientes</span>
         {waifus.map((waifu) => {
           return (
-            <div key={waifu.id} onClick={() => openModal(waifu.name, newKeyVote, waifu.id)} className="mt-4 cursor-pointer p-2 max-sm:w-[100%] sm:w-[100%] h-[170px] backdrop-blur-lg shadow-md rounded-md">
+            <div key={waifu.id} onClick={() => openModal(waifu.name, keyVote.current, waifu.id)} className="mt-4 cursor-pointer p-2 max-sm:w-[100%] sm:w-[100%] h-[170px] backdrop-blur-lg shadow-md rounded-md">
               <div className="flex flex-row items-start gap-4">
                 <Image src={`/${waifu.name}.jpeg`} alt='waifu image' width={100} height={100} className="rounded-full" />
                 <span className="bg-white p-1 rounded-sm font-semibold">{waifu.name}</span>
